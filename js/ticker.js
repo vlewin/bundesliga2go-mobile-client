f8495cc5b71da6322f7976b0112f4cb15e734df6
vlewin <vlewin@gmx.de>
2011-11-07 21:27:12 +0100

    - web sockets connection (ticker.js)

--- js/ticker.js
+++ js/ticker.js
@@ -1,20 +1,48 @@
-function getCurrentMatches(){
-  console.log("get running matches and periodically look for updates");
-  return true;
+function showResult(data) {
+  $('#tickerView').html(data);
+  console.log(data);
 }
 
-function getCommendMatches(){
-  console.log("get running matches and periodically look for updates");
-  return true;
+function wscheck() {
+  var ws = false;
+  if ("WebSocket" in window) {
+      ws = true;
+  }
+  return ws;
 }
 
-function showResult(){
-  return true;
+function connect(){
+  try{
+	  var socket;
+	  var host = "ws://foxhall.de:4040";
+    var socket = new WebSocket(host);
+    
+    socket.onopen = function() {
+      console.log("INFO: socket opened!");
+    }
+
+    socket.onmessage = function(msg) {
+      showResult(msg.data);
+    }
+
+    socket.onclose = function() {
+      console.log("INFO: socket closed!")
+    }			
+
+  } catch(exception) { 		 
+    console.log("ERROR: " + exception);
+  }
 }
 
-$.when(getCurrentMatches(), getCommendMatches())
-  .then(showResult)
-  .fail(function(){
-    console.error( 'one or more requests failed.' );
-});
-
+function ticker() {
+  $.when(wscheck(), connect())
+    .done(function(result){
+      console.log("WebSocket in window " + result);
+      return true;
+    })
+    
+    .fail(function(data){
+      console.info("HANDLE ERRORS!!!!");
+      return false;
+     });
+}
