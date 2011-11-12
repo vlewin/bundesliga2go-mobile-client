95dbaa80cc3bde17abc0ec8dc56acce3d630117e
vlewin <vlewin@gmx.de>
2011-11-12 21:35:39 +0100

    - continue with LiveTicker (logic steps)
    // 1. Get matches in progress and store in sessionStorage
    // 2. Get MaxGoalID from http://foxhall.de:8088/api/maxgoalid and save
    in localStorage
    // 3. Start listening for incoming goals (WebSocket) --- Fallback
    AJAX!!!
    //
    // Fallback: If none, show upcoming matches

--- js/ticker.js
+++ js/ticker.js
@@ -1,48 +1,159 @@
-function showResult(data) {
-  $('#tickerView').html(data);
-  console.log(data);
-}
+$.tickerQueue = function(args) {
+  var def = $.Deferred(function(dfd) {
+    var worker;
+    var failure=0;
 
-function wscheck() {
-  var ws = false;
-  if ("WebSocket" in window) {
-      ws = true;
-  }
-  return ws;
-}
+    if (window.Worker) {
+      console.log("AJAX call through web workers")
 
-function connect(){
-  try{
-	  var socket;
-	  var host = "ws://foxhall.de:4040";
-    var socket = new WebSocket(host);
-    
-    socket.onopen = function() {
-      console.log("INFO: socket opened!");
+      var worker = new Worker("js/ajax-worker.js");
+
+      worker.onmessage = function(event) {
+        if(event.data.type == "error") {
+          if(failure < 2) {
+            worker.postMessage(args.args);
+            failure++;
+          } else {
+            dfd.reject(event);
+          }
+        } else {
+          switch (event.data.key) {
+            case "maxGoalID":
+              console.log("MaxGoalID");
+              break;
+
+            case "matchday":
+              console.log("MaxGoalID");
+              break;
+
+            default:
+              console.log("ERROR: unknown key " + event.data.key)
+              break;
+          }
+          dfd.resolve();
+        }
+      };
+
+      worker.onerror = function(event) {
+        console.log("FAILED")
+        dfd.reject(event);
+      };
+
+      worker.postMessage(args.args); 
+
+    } else {
+      console.log("AJAX call in main thread")
+   
+      var request = $.getJSON(args.args.url);
+   
+      request.success(function(data) {
+        console.log("Request 200");  
+        dfd.resolve();
+      });
+
+      request.error(function(data) {
+        console.log("AJAX error!")
+        dfd.reject();
+      });
+
     }
+  });
 
-    socket.onmessage = function(msg) {
-      showResult(msg.data);
-    }
+  return def.promise();
+};
 
-    socket.onclose = function() {
-      console.log("INFO: socket closed!")
-    }			
-
-  } catch(exception) { 		 
-    console.log("ERROR: " + exception);
-  }
+function show(){
+  var html = "TEXT"
+  $('#tickerView').html(html);
 }
 
-function ticker() {
-  $.when(wscheck(), connect())
-    .done(function(result){
-      console.log("WebSocket in window " + result);
-      return true;
-    })
-    
-    .fail(function(data){
-      console.info("HANDLE ERRORS!!!!");
-      return false;
-     });
+
+function liveticker() {
+   
+  var sworker1 = $.seasonQueue({
+    file: 'js/ajax-worker.js',
+    args: { url: "http://foxhall.de:8088/api/maxgoalid", key: "maxGoalID" }
+  });
+
+  var sworker2 = $.seasonQueue({
+    file: 'js/ajax-worker.js',
+    args: { url: "http://foxhall.de:8088/api/matches/inprogress/", key: "matchesInProgress" }
+  });
+
+  $.when(sworker1, sworker2)
+    .done(function(result1){
+     console.log("Both workers are done, handle data from workers!");
+     show();
+     return true;
+   })
+
+   .fail(function(data){
+     console.info("HANDLE ERRORS!!!!");
+     return false;
+    });
 }
+
+// 1. Get matches in progress and save in sessionStorage
+// 2. Get MaxGoalID from http://foxhall.de:8088/api/maxgoalid and save in localStorage
+// 3. Start listening for incoming goals (WebSocket) --- Fallback AJAX!!!
+//
+// Fallback: If none, show upcoming matches
+
+
+//function showResult(data) {
+//  
+//  var request = $.getJSON("http://foxhall.de:8088/getGoalsSinceGoalID");
+//  
+//  $('#tickerView').html(data);
+//  console.log(data);
+//}
+//
+//function getMatchesInProgress() {
+//  
+//}
+//
+//function wscheck() {
+//  var ws = false;
+//  if ("WebSocket" in window) {
+//      ws = true;
+//  }
+//  return ws;
+//}
+//
+//function connect(){
+//  try{
+//	  var socket;
+//	  var host = "ws://foxhall.de:4040";
+//    var socket = new WebSocket(host);
+//    
+//    socket.onopen = function() {
+//      console.log("INFO: socket opened!");
+//    }
+//
+//    socket.onmessage = function(msg) {
+//      showResult(msg.data);
+//    }
+//
+//    socket.onclose = function() {
+//      console.log("INFO: socket closed!")
+//    }			
+//
+//  } catch(exception) { 		 
+//    console.log("ERROR: " + exception);
+//  }
+//}
+//
+//function ticker() {
+//  $.when(wscheck(), connect())
+//    .done(function(result){
+//      console.log("WebSocket in window " + result);
+//      return true;
+//    })
+//    
+//    .fail(function(data){
+//      console.info("HANDLE ERRORS!!!!");
+//      return false;
+//     });
+//}
+
+
